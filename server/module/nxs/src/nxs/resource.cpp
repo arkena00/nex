bbca337f8f30074c15d87284ec5bd82b5ee73d46
ads00 <adess00@hotmail.fr>
2016-10-26 06:39:15 +0200

    moving files

--- server/module/nxs/src/nxs/resource.cpp
+++ server/module/nxs/src/nxs/resource.cpp
@@ -2,20 +2,18 @@
 
 namespace nxs
 {
+    const auto& r = db::nex.resource;
 
     int resource::id() const { return _id; }
     const std::string& resource::name() const { return _name; }
 
     db::result resource::add(const std::string& name)
     {
-        const auto& r = db::nex.resource;
         return db::query() + (r.name = name, r.owner.id = 0);
     }
 
     db::result resource::get()
     {
-        const auto& r = db::nex.resource;
         return db::query() << (r.id, r.name);
     }
-
 } // nxs

638a6f44418d3a9f3ff46c5a3074d19762b51f90
ads00 <adess00@hotmail.fr>
2018-05-13 22:17:13 +0200

    [fix] QApplication exec

--- client/nxi/source/main.cpp
+++ client/nxi/source/main.cpp
@@ -8,17 +8,21 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     QMessageBox error;
+    int status = 0;
 
     try
     {
         nxi::core nxi_core;
         ui::core ui_core(app, nxi_core);
+
+        status = QApplication::exec();
     }
     catch (const std::exception& e)
     {
         error.setText(e.what());
         error.show();
+        status = QApplication::exec();
     }
 
-    return QApplication::exec();
+    return status;
 }

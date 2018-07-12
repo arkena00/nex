878a171798d0f6030eb8ca2f49faf7aadb992262
Feldrise <admin@feldrise.com>
2018-07-12 14:38:39 +0200

    [move] Visual Studio filters code in specific user config

--- system/include/nxs/version.hpp
+++ system/include/nxs/version.hpp
@@ -4,6 +4,6 @@
 namespace nxs_autoversion
 {
     static const char* version = "0.1";
-	static const char* fullversion = "0.1 - build 20180711151443";
+	static const char* fullversion = "0.1 - build 20180712123412";
 }
 #endif // CORE_VERSION_H_NXS

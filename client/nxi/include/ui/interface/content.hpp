6092a7b0cdccf2012fba3c5290fbaa40734a993a
ads00 <adess00@hotmail.fr>
2018-10-28 18:58:40 +0100

    [add] content interface

---
+++ client/nxi/include/ui/interface/content.hpp
@@ -0,0 +1,32 @@
+#ifndef UI_CONTENT_H_NXI
+#define UI_CONTENT_H_NXI
+
+#include <ui/interface.hpp>
+
+namespace nxw
+{
+    class web_view;
+    class explorer_view;
+} // nxw
+
+namespace ui { class core; }
+
+namespace ui::interfaces
+{
+    class content : public ui::interface
+    {
+    public:
+
+        explicit content(ui::core& ui_core);
+
+    private:
+        ui::core& ui_core_;
+
+        QStackedWidget* view_stack_;
+
+        nxw::web_view* web_view_;
+        nxw::explorer_view* explorer_view_;
+    };
+} // ui::interfaces
+
+#endif // UI_CONTENT_H_NXI
---
+++ client/nxi/source/ui/interface/content.cpp
@@ -0,0 +1,36 @@
+#include <ui/interface/content.hpp>
+
+#include <ui/core.hpp>
+#include <nxi/core.hpp>
+#include <nxi/system/command.hpp>
+
+#include <nxw/hbox_layout.hpp>
+#include <nxw/web_view.hpp>
+#include <nxw/explorer_view.hpp>
+
+namespace ui::interfaces
+{
+    content::content(ui::core& ui_core) :
+        ui_core_{ ui_core }
+    {
+        QHBoxLayout* layout = new nxw::hbox_layout;
+        setLayout(layout);
+
+        view_stack_ = new QStackedWidget(this);
+        web_view_ = new nxw::web_view(ui_core_);
+        explorer_view_ = new nxw::explorer_view(ui_core_);
+
+        view_stack_->addWidget(web_view_);
+        view_stack_->addWidget(explorer_view_);
+
+        layout->addWidget(view_stack_);
+
+        connect(&ui_core_.nxi_core().page_system(), QOverload<nxi::page&>::of(&nxi::page_system::event_change),
+                [this](nxi::page& page)
+                {
+                    //content_view_->context_update(page.type());
+                    if (page.type() == nxi::page_type::web) view_stack_->setCurrentIndex(0);
+                    if (page.type() == nxi::page_type::explorer) view_stack_->setCurrentIndex(1);
+                });
+    }
+} // ui::interfaces

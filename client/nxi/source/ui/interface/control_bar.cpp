0e0bda82e0df0acc85021ae918fb6f0242856d82
ads00 <adess00@hotmail.fr>
2018-11-07 14:46:25 +0100

    [update] control_bar interface

--- client/nxi/source/ui/interface/control_bar.cpp
+++ client/nxi/source/ui/interface/control_bar.cpp
@@ -21,17 +21,17 @@ namespace ui::interfaces
             auto layout = new nxw::hbox_layout;
             setLayout(layout);
 
-            auto win_minimize = new nxw::icon_button(this, "window_minimize");
+            auto win_minimize = new nxw::icon_button(this, ":/button/window_minimize");
             QObject::connect(win_minimize, &QPushButton::pressed, [this, &ui_core]()
             {
                 ui_core.window_system().minimize(window());
             });
 
-            auto win_close = new nxw::icon_button(this, "window_close");
+            auto win_close = new nxw::icon_button(this, ":/button/window_close");
             QObject::connect(win_close, &QPushButton::pressed, &ui_core, &ui::core::quit);
 
             layout->addWidget(win_minimize);
-            layout->addWidget(new nxw::icon_button(this, "window_maximize"));
+            layout->addWidget(new nxw::icon_button(this, ":/button/window_maximize"));
             layout->addWidget(win_close);
         }
     };
@@ -43,7 +43,7 @@ namespace ui::interfaces
         setLayout(layout);
         setStyleSheet("border-bottom:1px solid #CCCCCC;");
 
-        auto btn_menu = new nxw::icon_button(this, "menu");
+        auto btn_menu = new nxw::icon_button(this, ":/button/menu");
 
         /*
         class command
@@ -67,37 +67,15 @@ namespace ui::interfaces
         auto menu = new nxw::menu{ this };
         menu->setObjectName("main_menu");
         menu->add<nxw::menu_item>("new window", [&ui_core](){ ui_core.nxi_core().window_system().add({}); });
-        menu->add<nxw::menu_separator>();
-        auto z = new QComboBox;
-        z->addItem("test");
-        menu->add(z);
-        menu->add<nxw::menu_separator>();
         menu->add<nxw::menu_item>("options");
         menu->add<nxw::menu_item>("about");
         menu->add<nxw::menu_separator>();
-        menu->add<nxw::menu_item>("quit", [&ui_core](){ ui_core.quit(); }, ":/button/quit");
+        menu->add(ui_core.nxi_core().command_system().get("quit"));
         menu->show_at(btn_menu);
 
-        QObject::connect(btn_menu, &QPushButton::clicked, menu, &nxw::menu::exec);
+        QObject::connect(btn_menu, &nxw::icon_button::clicked, menu, &nxw::menu::exec);
         //menu->add(nxi::command::get("nxi", "quit"), customwidget);
 
-/*
-        QMenu* menu = new QMenu(this);
-        auto a_quit = new QAction("quit", menu);
-        a_quit->setText("quit");
-
-        auto a_win = new QAction(menu);
-        a_win->setText("new window");
-
-
-        QObject::connect(a_quit, &QAction::triggered, [this](){ m_ui_core.quit(); });
-        QObject::connect(a_win, &QAction::triggered, [this](){ m_ui_core.nxi_core().window_system().add({}); });
-
-        menu->addAction(a_win);
-        menu->addAction(a_quit);
-        */
-
-        //QObject::connect(btn_menu, &QPushButton::clicked, this, [menu]{ menu->exec(); });
 
         m_context = new QComboBox(this);
         m_context->addItem("auto");
@@ -114,19 +92,37 @@ namespace ui::interfaces
             m_ui_core.nxi_core().command_system().exec(m_address_bar->text(), context);
         });
 
+
+        // module command
+        auto module_controls = new nxw::hbox_layout;
+        for (const auto& command : m_ui_core.nxi_core().command_system().get())
+        {
+            auto button = new nxw::icon_button(this, command);
+            button->setText(command.command_name());
+            module_controls->addWidget(button);
+        }
+        QObject::connect(&ui_core.nxi_core().command_system(), &nxi::command_system::event_add, [&](const nxi::command& command)
+        {
+            auto button = new nxw::icon_button(this, command);
+            button->setText(command.command_name());
+            module_controls->addWidget(button);
+        });
+
+
         auto window_controls = new ui::interfaces::window_controls(ui_core);
 
         layout->addWidget(btn_menu);
         layout->addSpacing(64);
 
-        layout->addWidget(new nxw::icon_button(this, "history_previous"));
-        layout->addWidget(new nxw::icon_button(this, "history_next"));
-        layout->addWidget(new nxw::icon_button(this, "notification_none"));
-        layout->addWidget(new nxw::icon_button(this, "download"));
+        layout->addWidget(new nxw::icon_button(this, ":/button/history_previous"));
+        layout->addWidget(new nxw::icon_button(this, ":/button/history_next"));
+        layout->addWidget(new nxw::icon_button(this, ":/button/notification_none"));
+        layout->addWidget(new nxw::icon_button(this, ":/button/download"));
 
         layout->addSpacing(16);
         layout->addWidget(m_context);
         layout->addWidget(m_address_bar);
+        layout->addLayout(module_controls);
         layout->addStretch();
         layout->addWidget(window_controls);
     }
